c3a8fe50ae51b09b4a50ff01206dd940023d4538
snkmt <ruslic3d@gmail.com>
2026-06-18 20:19:06 +0300

    Зарегистрировал все модели в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Product, Attribute, AttributeValue, ProductVariant
 
-# Register your models here.
+class ProductVariantInline(admin.TabularInline):
+    model=ProductVariant
+    extra=1
+    filter_horizontal = ("attribute_values", )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "base_price", "is_active")
+    list_filter = ("category", "is_active")
+    search_fields = ("name", )
+    prepopulated_fields = {"slug": ("name", )}
+    inlines = [ProductVariantInline]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
+    prepopulated_fields = {"slug": ("name", )}
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+@admin.register(AttributeValue)
+class AttributeValueAdmin(admin.ModelAdmin):
+    list_display = ("attribute", "value")
+
